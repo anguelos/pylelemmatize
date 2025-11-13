06dd3b316a7d7a99338bf0f7c7a3e82d3da68fb9
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2025-11-13 06:04:42 +0100

    debugging test on github pytorch

--- test/pytest/test_torch_pylelemmatize.py
+++ test/pytest/test_torch_pylelemmatize.py
@@ -1,6 +1,7 @@
 import pylelemmatize_torch
 
 def test_torch_util_imported():
+    print("KEYS:", dir(pylelemmatize_torch).keys())
     assert pylelemmatize_torch is not None
     assert hasattr(pylelemmatize_torch, "Seq2SeqDs")
     assert hasattr(pylelemmatize_torch, "DemapperLSTM")
